df8db48acabd32f42d9e317d51a637c31015cfe1
Pavel Holec <pavel.holec@kiwi.com>
2023-05-17 16:24:51 +0200

    Fake user storing

--- TestApp/UserStorage/Sources/UserStorage/UserStoring.swift
+++ TestApp/UserStorage/Sources/UserStorage/UserStoring.swift
@@ -24,3 +24,19 @@ public extension EnvironmentValues {
     }
 }
 
+// TODO: Move to mocks target
+public final class FakeUserStorage: UserStoring {
+
+    @Published public var isLoggedIn = false
+    @Published public var bookings = [String]()
+
+    public var loggedInPublisher: AnyPublisher<Bool, Never> { $isLoggedIn.dropFirst().eraseToAnyPublisher() }
+    public var bookingsPublisher: AnyPublisher<[String], Never> { $bookings.dropFirst().eraseToAnyPublisher() }
+}
+
+public extension UserStoring where Self == FakeUserStorage {
+
+    static var mock: Self {
+        self.init()
+    }
+}

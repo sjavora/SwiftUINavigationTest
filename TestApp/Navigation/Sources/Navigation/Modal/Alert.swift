c3f87ab554e794a32ab3c0ac7807b43ace71c8c6
Šimon Javora <simon.javora@kiwi.com>
2023-06-02 13:08:13 +0200

    Updated presented view overloads.

--- TestApp/Navigation/Sources/Navigation/Modal/Alert.swift
+++ TestApp/Navigation/Sources/Navigation/Modal/Alert.swift
@@ -24,5 +24,28 @@ extension View {
         )
     }
 
-    // TODO: associated value variant
+    public func alert<Enum, Case>(
+        selection: Binding<Enum?>,
+        extract: @escaping (Enum) -> Case?,
+        embed: @escaping (Case) -> Enum,
+        onDismiss: (() -> Void)? = nil,
+        content: @escaping (Case) -> Alert
+    ) -> some View {
+        let binding = selection.case(extract: extract, embed: embed)
+
+        return ExternallyDismissableModal(
+            .alert,
+            isPresented: binding.isPresent(),
+            content: { isPresented in
+                self
+                    .alert(isPresented: isPresented) {
+                        guard let value = binding.wrappedValue else {
+                            fatalError("Alert binding's value is nil.")
+                        }
+
+                        return content(value)
+                    }
+            }
+        )
+    }
 }
--- TestApp/Navigation/Sources/Navigation/Modal/Sheet.swift
+++ TestApp/Navigation/Sources/Navigation/Modal/Sheet.swift
@@ -4,35 +4,6 @@ extension View {
 
     public func sheet<Value, Content>(
         selection: Binding<Value?>,
-        onDismiss: (() -> Void)? = nil,
-        @ViewBuilder content: @escaping (Value) -> Content
-    ) -> some View where Content: View {
-        ExternallyDismissableModal(.sheet, isPresented: selection.isPresent()) { isPresented in
-            self
-                .sheet(isPresented: isPresented, onDismiss: onDismiss) {
-                    Binding(unwrapping: selection).map { binding in
-                        content(binding.wrappedValue)
-                    }
-                }
-        }
-    }
-
-    public func sheet<Enum, Case, Content>(
-        selection: Binding<Enum?>,
-        extract: @escaping (Enum) -> Case?,
-        embed: @escaping (Case) -> Enum,
-        onDismiss: (() -> Void)? = nil,
-        @ViewBuilder content: @escaping (Case) -> Content
-    ) -> some View where Content: View {
-        sheet(
-            selection: selection.case(extract: extract, embed: embed),
-            onDismiss: onDismiss,
-            content: content
-        )
-    }
-
-    public func sheet<Value, Content>(
-        selection: Binding<Value?>,
         case: Value,
         onDismiss: (() -> Void)? = nil,
         @ViewBuilder content: @escaping () -> Content
@@ -54,18 +25,34 @@ extension View {
                 content()
             }
         )
+    }
 
-//        sheet(
-//            isPresented: Binding(
-//                get: { selection.wrappedValue == `case` },
-//                set: { isPresent, transaction in
-//                    if isPresent == false {
-//                        selection.transaction(transaction).wrappedValue = nil
-//                    }
-//                }
-//            ),
-//            onDismiss: onDismiss,
-//            content: content
-//        )
-    }    
+    public func sheet<Enum, Case, Content>(
+        selection: Binding<Enum?>,
+        extract: @escaping (Enum) -> Case?,
+        embed: @escaping (Case) -> Enum,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder content: @escaping (Case) -> Content
+    ) -> some View where Content: View {
+        sheet(
+            selection: selection.case(extract: extract, embed: embed),
+            onDismiss: onDismiss,
+            content: content
+        )
+    }
+
+    public func sheet<Value, Content>(
+        selection: Binding<Value?>,
+        onDismiss: (() -> Void)? = nil,
+        @ViewBuilder content: @escaping (Value) -> Content
+    ) -> some View where Content: View {
+        ExternallyDismissableModal(.sheet, isPresented: selection.isPresent()) { isPresented in
+            self
+                .sheet(isPresented: isPresented, onDismiss: onDismiss) {
+                    Binding(unwrapping: selection).map { binding in
+                        content(binding.wrappedValue)
+                    }
+                }
+        }
+    }
 }
